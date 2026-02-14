377d8afdd383ca7cb216dd475a79179c9fef634d
Dazy Baliyan <baliyandazy22@gmail.com>
2026-02-14 10:25:47 +0530

    Python Practice examples in program1.py

---
+++ Python-Basics/program1.py
@@ -0,0 +1,31 @@
+# Code 1
+name= "Dazy"
+age = 19
+print(name)
+print(age)
+
+# Code 2
+length= 10
+breadth=20
+print(length*breadth)
+
+# Code 3
+firstName= "Mohandas"
+secondName = "Karamchand"
+lastName = "Gandhi"
+print(firstName+" "+secondName +" "+lastName)
+
+# Code 4
+length =5
+breadth =10
+sum = length+breadth
+print(sum/2)
+
+# Code 5
+name =input("Enter your name ")
+print(name)
+
+# Code 6
+a = input("Enter the first number ")
+b = input("Enter the second number ")
+print(a + b)
